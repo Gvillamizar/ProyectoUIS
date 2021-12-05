7bd5357dac887c8183180bae2c5d6de355c1dc16
Cristina Villar <cristinavillar@gmail.com>
2021-12-04 19:54:48 -0500

    Ultimos Ajustes
    Ultimos Ajustes

--- Rockola/frontend/src/components/RegistroUsuario.js
+++ Rockola/frontend/src/components/RegistroUsuario.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Form, Button, Container, Row } from 'react-bootstrap'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 
 import { addUsuario } from '../apis/UsuariosCRUD'
 
 const RegistroUsuario = () => {
     var user = JSON.parse(localStorage.getItem("user"));
-    
-    function save(even) {     
-        even.preventDefault();   
+
+    function save(even) {
+        even.preventDefault();
         const obj = {
             Contraseña: even.target[0].value,
             Identificacion: even.target[1].value,
@@ -18,16 +18,16 @@ const RegistroUsuario = () => {
             Tipo_usuario: even.target[6].value,
             ID: user.uid,
         }
-        addUsuario(obj, (res)=>{
+        addUsuario(obj, (res) => {
             console.log(res);
-            if(res == "User Created"){
-               user.flagNewUser = false;
-               localStorage.setItem("user", JSON.stringify(user));
-               window.location.href="http://localhost:3000/home";
-            }else{
+            if (res == "User Created") {
+                user.flagNewUser = false;
+                localStorage.setItem("user", JSON.stringify(user));
+                window.location.href = "http://localhost:3000/home";
+            } else {
                 alert("Algo salió mal, vuelve a intentarlo")
-            }            
-        })                
+            }
+        })
     }
     return (
         <>
@@ -35,49 +35,58 @@ const RegistroUsuario = () => {
                 <h1>Registra Usuario</h1>
             </div>
             <Form onSubmit={save}>
-                <Form.Group className="mb-3" controlId="Nombre">
-                    <Form.Label>Nombre</Form.Label>
-                    <Form.Control type="text" placeholder="Ingresa tu nombre" />
-                </Form.Group>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="Nombre">
+                                <Form.Label>Nombre</Form.Label>
+                                <Form.Control type="text" placeholder="Ingresa tu nombre" />
+                            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="TipoUsuario">
-                    <Form.Label>Tipo Usuario:</Form.Label>
-                    <Form.Select>
-                        <option>Selecciona Tipo de Usuario</option>
-                        <option value="Administrador">Administrador</option>
-                        <option value="Cliente">Cliente</option>
-                    </Form.Select>
-                </Form.Group>
+                            <Form.Group className="mb-3" controlId="TipoUsuario">
+                                <Form.Label>Tipo Usuario:</Form.Label>
+                                <Form.Select>
+                                    <option>Selecciona Tipo de Usuario</option>
+                                    <option value="Administrador">Administrador</option>
+                                    <option value="Cliente">Cliente</option>
+                                </Form.Select>
+                            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="NombreEstablecimiento">
-                    <Form.Label>Nombre Establecimiento</Form.Label>
-                    <Form.Control type="tel" placeholder="Ingresa el nombre del establecimiento" />
-                </Form.Group>
+                            <Form.Group className="mb-3" controlId="NombreEstablecimiento">
+                                <Form.Label>Nombre Establecimiento</Form.Label>
+                                <Form.Control type="tel" placeholder="Ingresa el nombre del establecimiento" />
+                            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="NombreUsuario">
-                    <Form.Label>Nombre Usuario</Form.Label>
-                    <Form.Control type="text" placeholder="Ingresa el Nombre de Usuario" />
-                </Form.Group>
+                            <Form.Group className="mb-3" controlId="NombreUsuario">
+                                <Form.Label>Nombre Usuario</Form.Label>
+                                <Form.Control type="text" placeholder="Ingresa el Nombre de Usuario" />
+                            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="Contrasenha">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="text" laceholder="Ingresa la contraseña" />
-                </Form.Group>
+                        </Col>
+                        <Col>
 
-                <Form.Group className="mb-3" controlId="TipoIdentificacion">
-                    <Form.Label>Tipo Identificación:</Form.Label>
-                    <Form.Select>
-                        <option>Selecciona el Tipo de Identificación</option>
-                        <option value="Cédula Ciudadania">Cédula Ciudadania</option>
-                        <option value="Cédula Extrangeria">Cédula Extrangeria</option>
-                        <option value="Pasaporte">Pasaporte</option>
-                    </Form.Select>
-                </Form.Group>
+                            <Form.Group className="mb-3" controlId="Contrasenha">
+                                <Form.Label>Contraseña</Form.Label>
+                                <Form.Control type="text" laceholder="Ingresa la contraseña" />
+                            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="NumeroIdentificacion">
-                    <Form.Label>Numero Identificacion</Form.Label>
-                    <Form.Control type="text" placeholder="Ingresa el número de Identificación" />
-                </Form.Group>
+                            <Form.Group className="mb-3" controlId="TipoIdentificacion">
+                                <Form.Label>Tipo Identificación:</Form.Label>
+                                <Form.Select>
+                                    <option>Selecciona el Tipo de Identificación</option>
+                                    <option value="Cédula Ciudadania">Cédula Ciudadania</option>
+                                    <option value="Cédula Extrangeria">Cédula Extrangeria</option>
+                                    <option value="Pasaporte">Pasaporte</option>
+                                </Form.Select>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="NumeroIdentificacion">
+                                <Form.Label>Numero Identificacion</Form.Label>
+                                <Form.Control type="text" placeholder="Ingresa el número de Identificación" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </Container>
 
                 <Container fluid="md">
                     <Row>
